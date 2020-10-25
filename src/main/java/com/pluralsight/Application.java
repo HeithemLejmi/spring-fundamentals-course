00a801af5ac2c0ec5f61fe858d1b2d0bf55b1902
Heithem Lejmi <lejmi.heithem@gmail.com>
2020-10-25 20:38:08 +0100

    doc: add doc about web scopes for beans

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -9,12 +9,9 @@ public class Application {
     // Create the application context based on the @Configuration class : which creates the Spring IoC Container
     ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
     // The Bean "speakerService" will retrieve its dependency "speakerRepository" from this container
-    SpeakerService speakerService1 = context.getBean("speakerService", SpeakerService.class);
+    SpeakerService speakerService = context.getBean("speakerService", SpeakerService.class);
     // Here we can test that we really get the dependency injected inside "speakerService"
-    System.out.println(speakerService1.findAll().get(0).getFirstName());
-    SpeakerService speakerService2 = context.getBean("speakerService", SpeakerService.class);
-    System.out.println("Address of the 1st instance is : " + speakerService1);
-    System.out.println("Address of the 2nd instance is : " + speakerService2);
+    System.out.println(speakerService.findAll().get(0).getFirstName());
   }
 
 
