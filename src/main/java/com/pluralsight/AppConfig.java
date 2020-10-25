eba39827af09f53dc94e34dfc1042bf459e3d092
Heithem Lejmi <lejmi.heithem@gmail.com>
2020-10-25 22:14:08 +0100

    feat: use the stereotype annotations to fully autowire the spring beans

--- src/main/java/com/pluralsight/AppConfig.java
+++ src/main/java/com/pluralsight/AppConfig.java
@@ -1,27 +1,10 @@
 package com.pluralsight;
 
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
-import com.pluralsight.repository.SpeakerRepository;
-import com.pluralsight.service.SpeakerService;
-import com.pluralsight.service.SpeakerServiceImpl;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan
 public class AppConfig {
 
-  @Bean(name = "speakerService")
-  @Scope(value = BeanDefinition.SCOPE_SINGLETON)
-  public SpeakerService getSpeakerService(){
-    SpeakerServiceImpl speakerService = new SpeakerServiceImpl();
-    return speakerService;
-  }
-
-  @Bean(name = "speakerRepository")
-  @Scope(value = BeanDefinition.SCOPE_SINGLETON)
-  public SpeakerRepository getSpeakerRepository(){
-    return new HibernateSpeakerRepositoryImpl();
-  }
 }
