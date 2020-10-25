873a8e693eca981cb68951e62d22ffb3c281dfd0
Heithem Lejmi <lejmi.heithem@gmail.com>
2020-10-25 18:15:18 +0100

    feat: use setter injection

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,12 +1,19 @@
 package com.pluralsight;
 
 import com.pluralsight.service.SpeakerService;
-import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
   public static void main(String args[]){
-    SpeakerService service = new SpeakerServiceImpl();
-    System.out.println(service.findAll().get(0).getFirstName());
+    // Create the application context based on the @Configuration class : which creates the Spring IoC Container
+    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    // The Bean "speakerService" will retrieve its dependency "speakerRepository" from this container
+    SpeakerService speakerService = context.getBean("speakerService", SpeakerService.class);
+    // Here we can test that we really get the dependency injected inside "speakerService"
+    System.out.println(speakerService.findAll().get(0).getFirstName());
   }
+
+
 
 }
