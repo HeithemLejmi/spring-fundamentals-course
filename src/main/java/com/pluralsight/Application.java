bc9e86100727b37983b20d3ccb2bee6308bfe4dd
Heithem Lejmi <lejmi.heithem@gmail.com>
2020-10-25 20:07:52 +0100

    feat: define the singleton scope

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -9,9 +9,12 @@ public class Application {
     // Create the application context based on the @Configuration class : which creates the Spring IoC Container
     ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
     // The Bean "speakerService" will retrieve its dependency "speakerRepository" from this container
-    SpeakerService speakerService = context.getBean("speakerService", SpeakerService.class);
+    SpeakerService speakerService1 = context.getBean("speakerService", SpeakerService.class);
     // Here we can test that we really get the dependency injected inside "speakerService"
-    System.out.println(speakerService.findAll().get(0).getFirstName());
+    System.out.println(speakerService1.findAll().get(0).getFirstName());
+    SpeakerService speakerService2 = context.getBean("speakerService", SpeakerService.class);
+    System.out.println("Address of the 1st instance is : " + speakerService1);
+    System.out.println("Address of the 2nd instance is : " + speakerService2);
   }
 
 
